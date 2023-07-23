24cb35c3f52edfa488f3ed29d149f4915424a607
Jo Van den Berghe <jo.vandenberghe@smartphoto.com>
2023-07-23 18:28:32 +0200

    Add opencv.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // SSG
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false, path:false, "crypto": false  };
+        return config;
+    },
 }
 
 module.exports = nextConfig
